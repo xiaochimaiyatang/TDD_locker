7ed53d2aabb2f9189d58a2c54042a353dbf9437b
gezi Zhang <gezizhang@gezideMacBook-Pro.local>
2020-06-13 21:41:45 +0800

    add test:should_save_unsuccessfully_when_have_no_empty_box;

--- src/test/java/LockerTest.java
+++ src/test/java/LockerTest.java
@@ -1,4 +1,6 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -6,6 +8,8 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class LockerTest {
+    @Rule
+    public ExpectedException thrown=ExpectedException.none();
 
     @Test
     public void should_save_bag_successfully_when_have_empty_box() {
@@ -33,5 +37,23 @@ public class LockerTest {
         assertEquals(bag, myBag);
         assertEquals(2, locker.getAvailableBox());
 
+    }
+
+    @Test
+    public void should_save_unsuccessfully_when_have_no_empty_box(){
+//        given:
+        List<Box> box = Arrays.asList(new Box("001"), new Box("002"));
+        Locker locker = new Locker(box);
+        Bag myBag1 = new Bag();
+        Bag myBag2 = new Bag();
+        Ticket ticket1 = locker.save(myBag1);
+        Ticket ticket2 = locker.save(myBag2);
+        Bag bag=new Bag();
+//        when:
+        locker.save(bag);
+
+//        then:
+        thrown.expect(Exception.class);
+        thrown.expectMessage("save Unsuccefully, no Empty Box");
     }
 }
